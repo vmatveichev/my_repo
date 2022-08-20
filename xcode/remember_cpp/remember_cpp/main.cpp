4bdd32890c9b0d13cdd1fe93e45061a934b17a34
Viacheslav Matveichev <viacheslav.matveichev@gmail.com>
2022-08-20 10:55:01 +0200

    xcode folder added

---
+++ xcode/remember_cpp/remember_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  remember_cpp
+//
+//  Created by Viacheslav Matveichev on 2022-08-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
